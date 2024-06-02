48197a9ea7020606a220f06d3f7132d879ce678f
Yoshizawa Shogo <shogo.yoshizawa.2000@gmail.com>
2024-06-02 21:24:12 +0900

    仮完成

--- view_json_graph.py
+++ view_json_graph.py
@@ -5,16 +5,39 @@ from pprint import pprint
 
 def main():
     json_data = json.loads(input())
-    pprint(json_data)
+    display_welcome(json_data)
+
     sys.stdin = open('/dev/tty')
+
+    nodes = json_data.keys()
 
     node = ""
     hierarchy_list = []
     while True:
         order = input()
+
+        if len(hierarchy_list) == 0 and order not in nodes:
+            print("no such a node")
+            continue
+        if len(hierarchy_list) != 0:
+            if node not in nodes:
+                if order != '..':
+                    continue
+            elif order not in json_data[node] and order != '..':
+                print("no such a node")
+                continue
+
         clear_console()
-        node = order
-        hierarchy_list.append(node)
+
+        if order == '..':
+            hierarchy_list.pop()
+            if len(hierarchy_list) != 0:
+                node = hierarchy_list[-1]
+            else:
+                node = ''
+        else:
+            node = order
+            hierarchy_list.append(node)
 
         display_hierarchy(hierarchy_list)
         display_children(json_data, node, len(hierarchy_list) + 1)
@@ -31,10 +54,20 @@ def display_hierarchy(hierarchy_list):
     print(buff)
 
 def display_children(graph, parent_node, indent_level):
-    buff = ""
-    for node in graph[parent_node]:
-        buff += "  " * indent_level + node + "\n"
-    print(buff) 
+    buff = "  " * indent_level + "..\n"
+    if parent_node == '':
+        display_welcome(graph)
+        return
+    if parent_node not in graph:
+        print("## here is end node. ##")
+    else:
+        for node in graph[parent_node]:
+            buff += "  " * indent_level + node + "\n"
+    print(buff)
+
+def display_welcome(graph):
+    print("## select start node ##")
+    pprint(graph)
 
 if __name__ == '__main__':
     main()
